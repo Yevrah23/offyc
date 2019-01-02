7b5f8628adc964863acc65ff325f57c5806d7c82
jkpz10 <zayas.j10@gmail.com>
2019-01-03 07:32:59 +0800

    added modal and forms

--- src/app/pages/records/records.component.ts
+++ src/app/pages/records/records.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { SubmitProposalComponent } from 'src/app/modal/submit-proposal/submit-proposal.component';
 
 declare var $;
 @Component({
@@ -8,7 +10,10 @@ declare var $;
 })
 export class RecordsComponent implements OnInit {
 
-  // data tables
+  isAdmin = true;
+  isUser = false;
+  // Admin
+    // data tables
   @ViewChild('dataTableCOT') tableCOT: ElementRef;
   dataTableCOT: any;
   @ViewChild('dataTableCSM') tableCSM: ElementRef;
@@ -20,12 +25,35 @@ export class RecordsComponent implements OnInit {
   @ViewChild('dataTableCEA') tableCEA: ElementRef;
   dataTableCEA: any;
 
-  constructor() { }
+  // USER
+    // data tables
+  @ViewChild('user') tableUser: ElementRef;
+  user: any;
+
+  constructor( public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SubmitProposalComponent, {
+      width: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+    });
+  }
 
   ngOnInit(): void {
 
-    // Display data tables
+    // User
+      // Display data tables
       // COT
+    this.user = $(this.tableUser.nativeElement);
+    this.user.dataTable();
+
+    // Admin
+      // Display data tables
+        // COT
     this.dataTableCOT = $(this.tableCOT.nativeElement);
     this.dataTableCOT.dataTable();
       // CSM
