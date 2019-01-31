377552aa5eeba1fdcb88df0f8056c65fa3f4f7d3
WhiteBrim <jhbabia1998@gmail.com>
2019-01-31 11:12:40 +0800

    until implementation

--- src/app/modal/comment/comment.component.ts
+++ src/app/modal/comment/comment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { UserServices } from 'src/app/services/user_services';
 
 @Component({
   selector: 'app-comment',
@@ -7,14 +8,29 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  proposal_id: any;
+  comment: string;
 
   constructor(
     public dialog: MatDialog,
     public dialogRef: MatDialogRef<CommentComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private user: UserServices
   ) { }
 
   ngOnInit() {
+    this.proposal_id = this.data.data;
+  }
+
+  send_comment(){
+    console.log(this.proposal_id);
+    console.log(this.comment);
+
+    this.user.revise_proposal(this.proposal_id, this.comment).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    )
   }
 
 }
--- src/app/services/user_services.ts
+++ src/app/services/user_services.ts
@@ -77,6 +77,29 @@ export class UserServices {
             'budget_partner': params[0].budget_partner
         });
     }
+    update_proposal(params) {
+        // this.fileToGo = params[0].file;
+        console.log(params);
+        return this.http.post(this.serverUrl + 'codeigniter/api/Users/update_proposal', {
+            'title': params[0].title,
+            'date_start': params[0].date_start,
+            'date_end': params[0].date_end,
+            'b_target': params[0].b_target,
+            'b_gender': params[0].b_gender,
+            'venue': params[0].venue,
+            'trans_type': 1,
+            'token': params[0].token,
+            'fileName': params[0].filename,
+            'partner': params[0].partner,
+            'proponents': params[0].proponents,
+            'accre_level': params[0].accre_level,
+            'total_hours': params[0].total_hours,
+            'budget_ustp': params[0].budget_ustp,
+            'budget_partner': params[0].budget_partner,
+            'prop_id': params[0].prop_id,
+            
+        });
+    }
     proposal_approval(params){
         console.log(params);
         return this.http.post(this.serverUrl + 'codeigniter/api/Users/proposal_approval', {
@@ -96,6 +119,10 @@ export class UserServices {
         return this.http.post(this.serverUrl + 'codeigniter/api/Users/get_proposal',{'id':params});
     }
 
+    get_proposals_user(params){
+        return this.http.post(this.serverUrl + 'codeigniter/api/Users/get_proposals_user',{'user_id':params});
+    }
+
     getEvents(){
         return this.http.get(this.serverUrl + 'codeigniter/api/Users/get_events');
     }
@@ -111,26 +138,15 @@ export class UserServices {
         return this.http.post(this.serverUrl + 'codeigniter/api/Users/get_hemis', { 'quarter': params });
     }
 
+    revise_proposal(prop_id,comment){
+        return this.http.post(this.serverUrl + 'codeigniter/api/Users/revise_proposal', { 'prop_id': prop_id,'comment':comment });
+    }
+
     update_proj_stat(params){
         console.log(params);
         return this.http.post(this.serverUrl + 'codeigniter/api/Users/update_project_status', { 'id': params.id,'status':params.status });
     }
-    // getNotifs_user(){
-    //     return this.http.post(this.)
-    // }
-
-    // file_upload(){
-    //     console.log(this.fileToGo);
-    //     const formData = new FormData();
-    //     formData.append('file',
-    //         this.fileToGo);
-    //     // const headers = new HttpHeaders();
-    //     // headers.append('Content-Type', 'multipart/form-data');
-    //     // headers.append('Accept', 'application/json');
-    //     return this.http.post(this.serverUrl + 'codeigniter/api/Users/file_upload', {'file':formData,'file2':this.fileToGo.name});
-    // }
-    // logout(){
-    //     return this.http.
-    // }
-
+    implementation_status(status,prop_id){
+        return this.http.post(this.serverUrl + 'codeigniter/api/Users/implementation_status', { 'prop_id': prop_id, 'status': status });
+    }
 }
